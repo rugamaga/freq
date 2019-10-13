9f77b12996a93933602120fcbd7857c6fbe94c9a
rugamaga <rugamaga@gmail.com>
2019-10-14 03:38:15 +0900

    introduce structure Tokenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -21,24 +21,34 @@ Token* create_token(TokenType type, const char* buffer, size_t pos, size_t len) 
   return token;
 }
 
+typedef struct {
+  const char* buffer;
+  Token* current;
+  TokenizerState state;
+  size_t beg;
+  size_t pos;
+  size_t len;
+} Tokenizer;
+
 Token* tokenize(const char* buffer, size_t len) {
   // トークンは常に0文字目のTT_ROOTから
   // 始まってると考えることにして
   // 何かと楽をしましょう。
   Token* root = create_token(TT_ROOT, buffer, 0, 0);
 
-  // トークンをどんどん追加しながら処理しよう
-  Token* current = root;
-
   // ステートマシンとして全体の処理を行う
-  TokenizerState state = TS_EMPTY;
-  size_t pos = 0;
-  size_t beg = 0;
+  Tokenizer* tn = (Tokenizer*)malloc(sizeof(Tokenizer));
+  tn->buffer = buffer;
+  tn->current = root;
+  tn->state = TS_EMPTY;
+  tn->beg = 0;
+  tn->pos = 0;
+  tn->len = len;
 
-  while( pos < len ) {
-    const char c = buffer[pos];
+  while( tn->pos < tn->len ) {
+    const char c = tn->buffer[tn->pos];
 
-    if( state == TS_EMPTY ) {
+    if( tn->state == TS_EMPTY ) {
       // バッファの終了。
       // TS_EMPTYでバッファの終りが来るのが正常な終了。
       // なので読み取れたトークン情報のルートを返して終了
@@ -51,9 +61,9 @@ Token* tokenize(const char* buffer, size_t len) {
       // つまるところ「連続でスペースだったね…」ということ。
       // まだ何もトークンがないので読み飛ばす以外にすることはない
       if( c == ' ' || c == '\t' || c == '\r' || c == '\n' ) {
-        beg = pos;
-        pos += 1;
-        state = TS_EMPTY;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        tn->state = TS_EMPTY;
         continue;
       }
 
@@ -71,94 +81,94 @@ Token* tokenize(const char* buffer, size_t len) {
           c == '9'
       ) {
         // 1文字読んで数値解析モードに飛ぶ
-        beg = pos;
-        pos += 1;
-        state = TS_NUM;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        tn->state = TS_NUM;
         continue;
       }
 
       // TS_EMPTYでイコールが着たら、多分Equal。
       if( c == '=' ) {
-        beg = pos;
-        pos += 1;
-        state = TS_EQUAL;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        tn->state = TS_EQUAL;
         continue;
       }
 
       // TS_EMPTYでノットが着たら、多分Not。
       if( c == '!' ) {
-        beg = pos;
-        pos += 1;
-        state = TS_NOT;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        tn->state = TS_NOT;
         continue;
       }
 
       if( c == '+' ) {
-        beg = pos;
-        pos += 1;
-        Token* t = create_token(TT_PLUS, buffer, beg, pos - beg);
-        current->next = t;
-        current = t;
-        state = TS_EMPTY;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        Token* t = create_token(TT_PLUS, tn->buffer, tn->beg, tn->pos - tn->beg);
+        tn->current->next = t;
+        tn->current = t;
+        tn->state = TS_EMPTY;
         continue;
       }
 
       if( c == '-' ) {
-        beg = pos;
-        pos += 1;
-        Token* t = create_token(TT_MINUS, buffer, beg, pos - beg);
-        current->next = t;
-        current = t;
-        state = TS_EMPTY;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        Token* t = create_token(TT_MINUS, tn->buffer, tn->beg, tn->pos - tn->beg);
+        tn->current->next = t;
+        tn->current = t;
+        tn->state = TS_EMPTY;
         continue;
       }
 
       if( c == '*' ) {
-        beg = pos;
-        pos += 1;
-        Token* t = create_token(TT_MUL, buffer, beg, pos - beg);
-        current->next = t;
-        current = t;
-        state = TS_EMPTY;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        Token* t = create_token(TT_MUL, tn->buffer, tn->beg, tn->pos - tn->beg);
+        tn->current->next = t;
+        tn->current = t;
+        tn->state = TS_EMPTY;
         continue;
       }
 
       if( c == '/' ) {
-        beg = pos;
-        pos += 1;
-        Token* t = create_token(TT_DIV, buffer, beg, pos - beg);
-        current->next = t;
-        current = t;
-        state = TS_EMPTY;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        Token* t = create_token(TT_DIV, tn->buffer, tn->beg, tn->pos - tn->beg);
+        tn->current->next = t;
+        tn->current = t;
+        tn->state = TS_EMPTY;
         continue;
       }
 
       if( c == '(' ) {
-        beg = pos;
-        pos += 1;
-        Token* t = create_token(TT_LEFT_BRACKET, buffer, beg, pos - beg);
-        current->next = t;
-        current = t;
-        state = TS_EMPTY;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        Token* t = create_token(TT_LEFT_BRACKET, tn->buffer, tn->beg, tn->pos - tn->beg);
+        tn->current->next = t;
+        tn->current = t;
+        tn->state = TS_EMPTY;
         continue;
       }
 
       if( c == ')' ) {
-        beg = pos;
-        pos += 1;
-        Token* t = create_token(TT_RIGHT_BRACKET, buffer, beg, pos - beg);
-        current->next = t;
-        current = t;
-        state = TS_EMPTY;
+        tn->beg = tn->pos;
+        tn->pos += 1;
+        Token* t = create_token(TT_RIGHT_BRACKET, tn->buffer, tn->beg, tn->pos - tn->beg);
+        tn->current->next = t;
+        tn->current = t;
+        tn->state = TS_EMPTY;
         continue;
       }
 
       // TS_EMPTYでここまでの以外が来るのはまずいですよ！
-      fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が来てしまいました。\n", pos, c);
+      fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が来てしまいました。\n", tn->pos, c);
       exit(1);
     }
 
-    if( state == TS_NUM ) {
+    if( tn->state == TS_NUM ) {
       // 数字以外の文字が来たってことは、ここまでが数値リテラルだったってことでしょ？
       // TS_EMPTYで数値が始まったら、それは数値だ。
       if(
@@ -174,66 +184,66 @@ Token* tokenize(const char* buffer, size_t len) {
           c == '9'
       ) {
         // 引き続き数値として処理する
-        pos += 1;
+        tn->pos += 1;
         continue;
       }
 
       // 数字でない何かが来たということは、
       // この時点で数値リテラルは終わったということ。
       // ということでトークンにしてしまう
-      Token* t = create_token(TT_NUM, buffer, beg, pos - beg);
-      current->next = t;
-      current = t;
+      Token* t = create_token(TT_NUM, tn->buffer, tn->beg, tn->pos - tn->beg);
+      tn->current->next = t;
+      tn->current = t;
 
       // 文字の読み込みはしてないのでposは移動させなくていい
-      beg = pos;
+      tn->beg = tn->pos;
 
       // トークンを読み込んだので行き先状態はTS_EMPTY
-      state = TS_EMPTY;
+      tn->state = TS_EMPTY;
 
       continue;
     }
 
-    if( state == TS_EQUAL ) {
+    if( tn->state == TS_EQUAL ) {
       // ここでイコールがきたということは、
       // 同値判定のためのイコールだったということです
       if( c == '=' ) {
-        Token* t = create_token(TT_EQUAL, buffer, beg, pos - beg);
-        current->next = t;
-        current = t;
+        Token* t = create_token(TT_EQUAL, tn->buffer, tn->beg, tn->pos - tn->beg);
+        tn->current->next = t;
+        tn->current = t;
 
-        beg = pos;
-        pos += 1;
+        tn->beg = tn->pos;
+        tn->pos += 1;
         // トークンを読み込んだので行き先状態はTS_EMPTY
-        state = TS_EMPTY;
+        tn->state = TS_EMPTY;
         continue;
       }
 
       // 取り扱えない文字なのでエラーを出して落とす
-      fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", pos, c);
+      fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", tn->pos, c);
 
       // メモリ解放は頑張らない(D言語方式)
       // free_token(current);
       return NULL;
     }
 
-    if( state == TS_NOT ) {
+    if( tn->state == TS_NOT ) {
       // ここでイコールがきたということは、
       // 不等号だったということです
       if( c == '=' ) {
-        Token* t = create_token(TT_NOT_EQUAL, buffer, beg, pos - beg);
-        current->next = t;
-        current = t;
+        Token* t = create_token(TT_NOT_EQUAL, tn->buffer, tn->beg, tn->pos - tn->beg);
+        tn->current->next = t;
+        tn->current = t;
 
-        beg = pos;
-        pos += 1;
+        tn->beg = tn->pos;
+        tn->pos += 1;
         // トークンを読み込んだので行き先状態はTS_EMPTY
-        state = TS_EMPTY;
+        tn->state = TS_EMPTY;
         continue;
       }
 
       // 取り扱えない文字なのでエラーを出して落とす
-      fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", pos, c);
+      fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", tn->pos, c);
 
       // メモリ解放は頑張らない(D言語方式)
       // free_token(current);
@@ -241,7 +251,7 @@ Token* tokenize(const char* buffer, size_t len) {
     }
 
     // 取り扱えない文字なのでエラーを出して落とす
-    fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", pos, c);
+    fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", tn->pos, c);
 
     // メモリ解放は頑張らない(D言語方式)
     // free_token(current);
