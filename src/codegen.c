89a2e870378fc0eb90059c186cea32ba50399c86
rugamaga <rugamaga@gmail.com>
2019-10-13 20:07:48 +0900

    Split codegen code

--- src/codegen.c
+++ src/codegen.c
@@ -19,6 +19,34 @@ static void code(CodeGen* gen, const char* format, ...) {
   va_end(va);
 }
 
+static size_t generate_alloca(CodeGen* gen) {
+  const size_t mem = ++(gen->index);
+  code(gen, "  %%%zu = alloca i32, align 4\n", mem);
+  return mem;
+}
+
+static void generate_store(CodeGen* gen, size_t reg, size_t mem) {
+  code(gen, "  store i32 %%%zu, i32* %%%zu, align 4\n", reg, mem);
+}
+
+static size_t generate_load(CodeGen* gen, size_t src) {
+  const size_t dst = ++(gen->index);
+  code(gen, "  %%%zu = load i32, i32* %%%zu, align 4\n", dst, src);
+  return dst;
+}
+
+static size_t generate_add(CodeGen* gen, size_t lhs, size_t rhs) {
+  const size_t reg = ++(gen->index);
+  code(gen, "  %%%zu = add i32 %%%zu, %%%zu\n", reg, lhs, rhs);
+  return reg;
+}
+
+static size_t generate_sub(CodeGen* gen, size_t lhs, size_t rhs) {
+  const size_t reg = ++(gen->index);
+  code(gen, "  %%%zu = sub i32 %%%zu, %%%zu\n", reg, lhs, rhs);
+  return reg;
+}
+
 static size_t generate_numeric_process(CodeGen* gen, AST* ast) {
   if (ast->type == ST_NUM) {
     const size_t alloc_index = ++(gen->index);
@@ -44,39 +72,21 @@ static size_t generate_numeric_process(CodeGen* gen, AST* ast) {
   case ST_ADD:
     {
       code(gen, "  ; ------------- Calculate ST_ADD\n");
-
-      const size_t lhs_reg_index = ++(gen->index);
-      code(gen, "  %%%zu = load i32, i32* %%%zu, align 4\n", lhs_reg_index, lhs_index);
-
-      const size_t rhs_reg_index = ++(gen->index);
-      code(gen, "  %%%zu = load i32, i32* %%%zu, align 4\n", rhs_reg_index, rhs_index);
-
-      const size_t add_reg_index = ++(gen->index);
-      code(gen, "  %%%zu = add i32 %%%zu, %%%zu\n", add_reg_index, lhs_reg_index, rhs_reg_index);
-
-      const size_t res_mem_index = ++(gen->index);
-      code(gen, "  %%%zu = alloca i32, align 4\n", res_mem_index);
-
-      code(gen, "  store i32 %%%zu, i32* %%%zu, align 4\n", add_reg_index, res_mem_index);
+      const size_t lhs_reg_index = generate_load(gen, lhs_index);
+      const size_t rhs_reg_index = generate_load(gen, rhs_index);
+      const size_t add_reg_index = generate_add(gen, lhs_reg_index, rhs_reg_index);
+      const size_t res_mem_index = generate_alloca(gen);
+      generate_store(gen, add_reg_index, res_mem_index);
     }
     break;
   case ST_SUB:
     {
       code(gen, "  ; ------------- Calculate ST_ADD\n");
-
-      const size_t lhs_reg_index = ++(gen->index);
-      code(gen, "  %%%zu = load i32, i32* %%%zu, align 4\n", lhs_reg_index, lhs_index);
-
-      const size_t rhs_reg_index = ++(gen->index);
-      code(gen, "  %%%zu = load i32, i32* %%%zu, align 4\n", rhs_reg_index, rhs_index);
-
-      const size_t sub_reg_index = ++(gen->index);
-      code(gen, "  %%%zu = sub i32 %%%zu, %%%zu\n", sub_reg_index, lhs_reg_index, rhs_reg_index);
-
-      const size_t res_mem_index = ++(gen->index);
-      code(gen, "  %%%zu = alloca i32, align 4\n", res_mem_index);
-
-      code(gen, "  store i32 %%%zu, i32* %%%zu, align 4\n", sub_reg_index, res_mem_index);
+      const size_t lhs_reg_index = generate_load(gen, lhs_index);
+      const size_t rhs_reg_index = generate_load(gen, rhs_index);
+      const size_t sub_reg_index = generate_sub(gen, lhs_reg_index, rhs_reg_index);
+      const size_t res_mem_index = generate_alloca(gen);
+      generate_store(gen, sub_reg_index, res_mem_index);
     }
     break;
   }
