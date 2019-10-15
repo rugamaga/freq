44295d051d8f1e18da17be32030f2f36ab79a327
rugamaga <rugamaga@gmail.com>
2019-10-16 00:23:07 +0900

    to prepare generalize, define alpha-type variables

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -76,6 +77,11 @@ static void into(Tokenizer* tn, TokenizerState state) {
   tn->state = state;
 }
 
+static void error(Tokenizer* tn, char c) {
+  fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", tn->pos, c);
+  exit(EXIT_FAILURE);
+}
+
 Token* tokenize(const char* buffer, size_t len) {
   Tokenizer* tn = create_tokenizer(buffer, len);
 
@@ -85,11 +91,32 @@ Token* tokenize(const char* buffer, size_t len) {
   ) {
     const char c = tn->buffer[tn->pos];
 
+    const bool is_null = ('\0' == c);
+    const bool is_space = (' ' == c || '\t' == c || '\r' == c || '\n' == c);
+    const bool is_l = ('l' == c);
+    const bool is_e = ('e' == c);
+    const bool is_t = ('t' == c);
+    const bool is_equal = ('=' == c);
+    const bool is_bang = ('!' == c);
+    const bool is_lt = ('<' == c);
+    const bool is_gt = ('>' == c);
+    const bool is_plus = ('+' == c);
+    const bool is_minus = ('-' == c);
+    const bool is_star = ('*' == c);
+    const bool is_slash = ('/' == c);
+    const bool is_left_paren = ('(' == c);
+    const bool is_right_paren = (')' == c);
+    const bool is_semicolon = (';' == c );
+    const bool is_lower = ('a' <= c && c <= 'z');
+    const bool is_upper = ('A' <= c && c <= 'Z');
+    const bool is_digit = ('0' <= c && c <= '9');
+    const bool is_default = true;
+
     if( tn->state == TS_EMPTY ) {
       // バッファの終了。
       // TS_EMPTYでバッファの終りが来るのが正常な終了。
       // なので読み取れたトークン情報のルートを返して終了
-      if( c == '\0' ) {
+      if( is_null ) {
         gain(tn);
         accept(tn, TT_EOF);
         // 終了状態にintoするので終了となる
@@ -101,7 +128,7 @@ Token* tokenize(const char* buffer, size_t len) {
       // しかしTS_EMPTYモードでのスペースやトークンとは、
       // つまるところ「連続でスペースだったね…」ということ。
       // まだ何もトークンがないので読み飛ばす以外にすることはない
-      if( c == ' ' || c == '\t' || c == '\r' || c == '\n' ) {
+      if( is_space ) {
         gain(tn);
         accept(tn, TT_SKIP);
         into(tn, TS_EMPTY);
@@ -109,20 +136,20 @@ Token* tokenize(const char* buffer, size_t len) {
       }
 
       // 'l'が来たならTT_LETっぽさがある
-      if( c == 'l' ) {
+      if( is_l ) {
         gain(tn);
         into(tn, TS_LET_0);
         continue;
       }
 
-      if( 'a' <= c && c <= 'z' ) {
+      if( is_lower ) {
         gain(tn);
         into(tn, TS_IDENT);
         continue;
       }
 
       // TS_EMPTYで数値が始まったら、それは数値だ。
-      if( '0' <= c && c <= '9' ) {
+      if( is_digit ) {
         // 1文字読んで数値解析モードに飛ぶ
         gain(tn);
         into(tn, TS_NUM);
@@ -130,122 +157,120 @@ Token* tokenize(const char* buffer, size_t len) {
       }
 
       // TS_EMPTYでイコールが着たら、多分Equal。
-      if( c == '=' ) {
+      if( is_equal ) {
         gain(tn);
         into(tn, TS_EQUAL);
         continue;
       }
 
       // TS_EMPTYでノットが着たら、多分Not。
-      if( c == '!' ) {
+      if( is_bang ) {
         gain(tn);
         into(tn, TS_NOT);
         continue;
       }
 
       // TS_EMPTYでLTが着たら、多分LT
-      if( c == '<' ) {
+      if( is_lt ) {
         gain(tn);
         into(tn, TS_LT);
         continue;
       }
 
-      // TS_EMPTYでLTが着たら、多分LT
-      if( c == '>' ) {
+      // TS_EMPTYでGTが着たら、多分GT
+      if( is_gt ) {
         gain(tn);
         into(tn, TS_GT);
         continue;
       }
 
-      if( c == '+' ) {
+      if( is_plus ) {
         gain(tn);
         accept(tn, TT_PLUS);
         into(tn, TS_EMPTY);
         continue;
       }
 
-      if( c == '-' ) {
+      if( is_minus ) {
         gain(tn);
         accept(tn, TT_MINUS);
         into(tn, TS_EMPTY);
         continue;
       }
 
-      if( c == '*' ) {
+      if( is_star ) {
         gain(tn);
         accept(tn, TT_MUL);
         into(tn, TS_EMPTY);
         continue;
       }
 
-      if( c == '/' ) {
+      if( is_slash ) {
         gain(tn);
         accept(tn, TT_DIV);
         into(tn, TS_EMPTY);
         continue;
       }
 
-      if( c == '(' ) {
+      if( is_left_paren ) {
         gain(tn);
         accept(tn, TT_LEFT_BRACKET);
         into(tn, TS_EMPTY);
         continue;
       }
 
-      if( c == ')' ) {
+      if( is_right_paren ) {
         gain(tn);
         accept(tn, TT_RIGHT_BRACKET);
         into(tn, TS_EMPTY);
         continue;
       }
 
-      if( c == ';' ) {
+      if( is_semicolon ) {
         gain(tn);
         accept(tn, TT_SEMICOLON);
         into(tn, TS_EMPTY);
         continue;
       }
 
-      // TS_EMPTYでここまでの以外が来るのはまずいですよ！
-      fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が来てしまいました。\n", tn->pos, c);
-      exit(1);
+      if( is_default ) {
+        error(tn, c);
+        continue;
+      }
     }
 
     if( tn->state == TS_LET_0 ) {
       // 'e'が来るならlet節っぽさが増す
-      if( c == 'e' ) {
+      if( is_e ) {
         gain(tn);
         into(tn, TS_LET_1);
         continue;
       }
 
-      if(
-          ('a' <= c && c <= 'z') ||
-          ('0' <= c && c <= '9')
-      ) {
+      if( is_lower || is_digit ) {
         gain(tn);
         into(tn, TS_IDENT);
         continue;
       }
 
       // それ以外の文字が来たので打ち切り
-      accept(tn, TT_IDENT);
-      into(tn, TS_EMPTY);
+      if( is_default ) {
+        accept(tn, TT_IDENT);
+        into(tn, TS_EMPTY);
+        continue;
+      }
     }
 
     if( tn->state == TS_LET_1 ) {
       // 't'が来るならlet節っぽさが更に増す
-      if( c == 't' ) {
+      if( is_t ) {
         gain(tn);
         into(tn, TS_LET_2);
         continue;
       }
 
       // そうじゃないならIDENTだったんだよ。
-      if(
-          ('a' <= c && c <= 'z') ||
-          ('0' <= c && c <= '9')
-      ) {
+      if( is_lower || is_digit ) {
         gain(tn);
         into(tn, TS_IDENT);
         continue;
@@ -254,33 +279,35 @@ Token* tokenize(const char* buffer, size_t len) {
       // それら以外なら
       // ここまでIDENTを処理していて
       // そのIDENTが今終わったのだ
-      accept(tn, TT_IDENT);
-      into(tn, TS_EMPTY);
+      if( is_default ) {
+        accept(tn, TT_IDENT);
+        into(tn, TS_EMPTY);
+        continue;
+      }
     }
 
     if( tn->state == TS_LET_2 ) {
       // ident的な文字が来てしまうなら
       // 実はletじゃなくてidentだったので
       // そのままidentとしてがんばってもらう
-      if(
-          ('a' <= c && c <= 'z') ||
-          ('0' <= c && c <= '9')
-      ) {
+      if( is_lower || is_digit ) {
         gain(tn);
         into(tn, TS_IDENT);
         continue;
       }
 
       // それ以外の文字が来たのでついにletであることが判明！
-      accept(tn, TT_LET);
-      into(tn, TS_EMPTY);
-      continue;
+      if( is_default ) {
+        accept(tn, TT_LET);
+        into(tn, TS_EMPTY);
+        continue;
+      }
     }
 
     if( tn->state == TS_NUM ) {
       // 数字以外の文字が来たってことは、ここまでが数値リテラルだったってことでしょ？
       // TS_EMPTYで数値が始まったら、それは数値だ。
-      if( '0' <= c && c <= '9' ) {
+      if( is_digit ) {
         // 引き続き数値として処理する
         gain(tn);
         into(tn, TS_NUM);
@@ -290,10 +317,12 @@ Token* tokenize(const char* buffer, size_t len) {
       // 数字でない何かが来たということは、
       // この時点で数値リテラルは終わったということ。
       // ということでトークンにしてしまう
-      accept(tn, TT_NUM);
+      if( is_default ) {
+        accept(tn, TT_NUM);
+        into(tn, TS_EMPTY);
+      }
 
       // トークンを読み込んだので行き先状態はTS_EMPTY
-      into(tn, TS_EMPTY);
 
       continue;
     }
@@ -301,7 +330,7 @@ Token* tokenize(const char* buffer, size_t len) {
     if( tn->state == TS_EQUAL ) {
       // ここでイコールがきたということは、
       // 同値判定のためのイコールだったということです
-      if( c == '=' ) {
+      if( is_equal ) {
         gain(tn);
         accept(tn, TT_EQUAL);
         // トークンを読み込んだので行き先状態はTS_EMPTY
@@ -310,15 +339,17 @@ Token* tokenize(const char* buffer, size_t len) {
       }
 
       // そうでにないなら、これは代入演算子なのでは？
-      accept(tn, TT_ASSIGN);
-      into(tn, TS_EMPTY);
-      continue;
+      if( is_default ) {
+        accept(tn, TT_ASSIGN);
+        into(tn, TS_EMPTY);
+        continue;
+      }
     }
 
     if( tn->state == TS_NOT ) {
       // ここでイコールがきたということは、
       // 不等号だったということです
-      if( c == '=' ) {
+      if( is_equal ) {
         gain(tn);
         accept(tn, TT_NOT_EQUAL);
         // トークンを読み込んだので行き先状態はTS_EMPTY
@@ -327,17 +358,16 @@ Token* tokenize(const char* buffer, size_t len) {
       }
 
       // 取り扱えない文字なのでエラーを出して落とす
-      fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", tn->pos, c);
-
-      // メモリ解放は頑張らない(D言語方式)
-      // free_token(current);
-      return NULL;
+      if( is_default ) {
+        error(tn, c);
+        continue;
+      }
     }
 
     if( tn->state == TS_LT ) {
       // ここでイコールがきたということは、
       // LTEQだったということです
-      if( c == '=' ) {
+      if( is_equal ) {
         gain(tn);
         accept(tn, TT_LTEQ);
         // トークンを読み込んだので行き先状態はTS_EMPTY
@@ -347,16 +377,17 @@ Token* tokenize(const char* buffer, size_t len) {
 
       // それ以外の文字が来たということは
       // LTだったということです
-      accept(tn, TT_LT);
-      into(tn, TS_EMPTY);
-
-      continue;
+      if( is_default ) {
+        accept(tn, TT_LT);
+        into(tn, TS_EMPTY);
+        continue;
+      }
     }
 
     if( tn->state == TS_GT ) {
       // ここでイコールがきたということは、
       // GTEQだったということです
-      if( c == '=' ) {
+      if( is_equal ) {
         gain(tn);
         accept(tn, TT_GTEQ);
         // トークンを読み込んだので行き先状態はTS_EMPTY
@@ -366,32 +397,31 @@ Token* tokenize(const char* buffer, size_t len) {
 
       // それ以外の文字が来たということは
       // GTだったということです
-      accept(tn, TT_GT);
-      into(tn, TS_EMPTY);
+      if( is_default ) {
+        accept(tn, TT_GT);
+        into(tn, TS_EMPTY);
+      }
 
       continue;
     }
 
     if( tn->state == TS_IDENT ) {
-      if(
-        // 引き続き文字がきているようならIDENT
-        ('a' <= c && c <= 'z') ||
-        // 2文字目以降は数字も許す
-        ('0' <= c && c <= '9')
-      ) {
+      if( is_lower || is_digit ) {
         gain(tn);
         into(tn, TS_IDENT);
         continue;
       }
 
       // それ以外の文字が来たので打ち切り
-      accept(tn, TT_IDENT);
-      into(tn, TS_EMPTY);
-      continue;
+      if( is_default ) {
+        accept(tn, TT_IDENT);
+        into(tn, TS_EMPTY);
+        continue;
+      }
     }
 
     // 取り扱えない文字なのでエラーを出して落とす
-    fprintf(stderr, "Tokenize中に予想外の文字(%zu文字目の'%c')が着てしまいました。\n", tn->pos, c);
+    error(tn, c);
 
     // メモリ解放は頑張らない(D言語方式)
     // free_token(current);
